a2b3e1bb89c06e0b4b4af63c64e5f22e4c6009ed
admin <admin@islam-salama.com>
2023-02-14 14:31:28 +0000

    first commit

---
+++ event/views.py
@@ -0,0 +1,14 @@
+from locale import locale_encoding_alias
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from django.template import loader
+from django.http import HttpResponse, JsonResponse
+from django import template
+from .models import *
+
+# Create your views here.
+def index(request):
+    context_object_name = 'index'
+    context={}
+    html_template = loader.get_template( 'frontend/index.html' )
+    return HttpResponse(html_template.render(context, request))
