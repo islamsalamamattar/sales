98c30d54dc17d81f205c792c63c3cff688ad4bf7
islamsalamamattar <islam-salama@hotmail.com>
2023-02-15 23:41:30 +0200

    updating models

--- customer/admin.py
+++ customer/admin.py
@@ -3,25 +3,25 @@ from .models import *
 
 #Cities > Areas admin
 class AreaInlines(admin.TabularInline):
-        model = Model
+        model = Area
 class CityAdmin(admin.ModelAdmin):
         list_display = ("name",)
         inlines = [AreaInlines]
 
 class CustomerInlines(admin.TabularInline):
-        model = Model
+        model = Customer
 class CustomerAdmin(admin.ModelAdmin):
         list_display = ("name", "area")
         inlines = [CustomerInlines]
 
 class DiscountInlines(admin.TabularInline):
-        model = Model
+        model = Discount
 class DiscountAdmin(admin.ModelAdmin):
         list_display = ("customer", "product", "discount")
         inlines = [DiscountInlines]
 
 class InvoiceInlines(admin.TabularInline):
-        model = Model
+        model = Invoice
 class InvoiceItemAdmin(admin.ModelAdmin):
         list_display = ("invoce", "product", "quantity")
         inlines = [InvoiceInlines]
