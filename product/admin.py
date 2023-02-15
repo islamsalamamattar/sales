ad24df704e0f814c9338b0c6eed9e56e2bcc183e
islamsalamamattar <islam-salama@hotmail.com>
2023-02-15 23:49:36 +0200

    updating appAdmins

--- product/admin.py
+++ product/admin.py
@@ -1,21 +1,15 @@
 from django.contrib import admin
 from .models import *
-# Register your models here.
+
 #Brands > Models admin
-class ModelInline(admin.TabularInline):
-        model = Brand
+class ProductInline(admin.TabularInline):
+        model = Product
 class BrandsAdmin(admin.ModelAdmin):
         list_display = ("name",)
         search_fields = ["name",]
-        inlines = [ ModelInline,]
+        inlines = [ ProductInline,]
 
-class ModelInline(admin.TabularInline):
-        model = Product
-class ProductsAdmin(admin.ModelAdmin):
-        list_display = ("name",)
-        search_fields = ["name",]
-        inlines = [ ModelInline,]
+
 
 
 admin.site.register(Brand, BrandsAdmin)
-admin.site.register(Product, ProductsAdmin)
